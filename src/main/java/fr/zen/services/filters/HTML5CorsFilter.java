61d550f72def9a7c531169fffb770396e7a7b363
Alexandre Jarnoux <alexandre.jarnoux@beesearch.fr>
2013-04-19 14:55:42 +0200

    Test du filter cross-domain

---
+++ src/main/java/fr/zen/services/filters/HTML5CorsFilter.java
@@ -0,0 +1,39 @@
+package fr.zen.services.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName = "HTML5CorsFilter", urlPatterns = { "/rest/*" })
+public class HTML5CorsFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("###### HTML5CorsFilter.init");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		System.out.println("###### HTML5CorsFilter.doFilter add HTML5 CORS Headers");
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("###### HTML5CorsFilter.destroy");
+	}
+
+}
